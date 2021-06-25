f46e3e174ddec4dae557f8e3f3a7173d5e6c38fe
itsamankrsingh <itsamankrsingh@gmail.com>
2021-06-26 00:29:11 +0530

    Initial commit

---
+++ toasty/src/main/java/com/itsamankrsingh/toasty/Toaster.kt
@@ -0,0 +1,15 @@
+package com.itsamankrsingh.toasty
+
+import android.content.Context
+import android.widget.Toast
+
+
+class Toaster {
+companion object{
+    fun simpleToast(context: Context, msg: String) {
+        Toast.makeText(context, msg, Toast.LENGTH_LONG ).show()
+    }
+}
+
+
+}
